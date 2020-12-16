ed3b2818bab7edcd8bd24a3fc046304ae153b775
Jakub.Porochonski <Jakub.Porochonski@JPTSGLAPTOP>
2020-12-16 19:40:31 +0000

    Implemented receiving notification from firebase

---
+++ MobileAppPlayground/MobileAppPlayground/MobileAppPlayground.Android/CustomFirebaseMessagingService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Util;
+using AndroidX.Core.App;
+using Firebase.Messaging;
+
+namespace MobileAppPlayground.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class CustomFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "CustomFirebaseMessagingService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            var body = message.GetNotification().Body;
+            Log.Debug(TAG, "Notification Message Body: " + body); 
+            SendNotification(body, message.Data);
+        }
+
+        public override void OnNewToken(string p0)
+        {
+            base.OnNewToken(p0);
+
+            Log.Debug(TAG, "Refreshed token: " + p0);
+
+            // Send token to 360 api
+        }
+
+        void SendNotification(string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (var key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+
+            var pendingIntent = PendingIntent.GetActivity(this,
+                MainActivity.NOTIFICATION_ID,
+                intent,
+                PendingIntentFlags.OneShot);
+            
+            var notificationBuilder = new NotificationCompat.Builder(this, MainActivity.CHANNEL_ID)
+                .SetSmallIcon(Resource.Drawable.navigation_empty_icon)
+                .SetContentTitle("FCM Message")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
--- MobileAppPlayground/MobileAppPlayground/MobileAppPlayground.Android/MainActivity.cs
+++ MobileAppPlayground/MobileAppPlayground/MobileAppPlayground.Android/MainActivity.cs
@@ -1,14 +1,7 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using Android.Gms.Common;
-using Firebase.Messaging;
-using Firebase.Iid;
 using Android.Util;
 
 namespace MobileAppPlayground.Droid
@@ -16,22 +9,78 @@ namespace MobileAppPlayground.Droid
     [Activity(Label = "MobileAppPlayground", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.OnCreate(savedInstanceState);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                }
+            }
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            IsPlayServicesAvailable();
+
+            CreateNotificationChannel();
+            
+           // Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Error(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Error(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                Log.Error(TAG, "Google Play Services is available.");
+                return true;
+            }
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                "FCM Notifications",
+                NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
